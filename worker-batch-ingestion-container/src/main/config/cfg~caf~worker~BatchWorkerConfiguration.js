9e8f0773b670c3f229bb6568d8f5261f3267dd53
Davide Giorgio Picchione <davide-giorgio.picchione@hpe.com>
2019-04-03 10:24:57 +0100

    Revert "Removed pointless cache time setting"
    This reverts commit db965c9916f1ede5ea63c8b19a6038389031a256.

--- worker-batch-ingestion-container/src/main/config/cfg~caf~worker~BatchWorkerConfiguration.js
+++ worker-batch-ingestion-container/src/main/config/cfg~caf~worker~BatchWorkerConfiguration.js
@@ -20,5 +20,6 @@
             || getenv("CAF_WORKER_OUTPUT_QUEUE")
             || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-out",
     threads: getenv("CAF_WORKER_THREADS") || 1,
+    cacheExpireTime: getenv("CAF_BATCH_WORKER_CACHE_EXPIRE_TIME") || 120,
     returnValueBehaviour: getenv("CAF_BATCH_WORKER_ERROR_QUEUE") ? "RETURN_NONE" : undefined
 });
