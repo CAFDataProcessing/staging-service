530295c07d416ffab7d7e6178e8b1274a132a228
Davide Giorgio Picchione <davide-giorgio.picchione@hpe.com>
2019-04-03 15:09:22 +0100

    SCMOD-5963: set default cache expire time

--- worker-batch-ingestion-container/src/main/config/cfg~caf~worker~BatchWorkerConfiguration.js
+++ worker-batch-ingestion-container/src/main/config/cfg~caf~worker~BatchWorkerConfiguration.js
@@ -20,6 +20,6 @@
             || getenv("CAF_WORKER_OUTPUT_QUEUE")
             || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-out",
     threads: getenv("CAF_WORKER_THREADS") || 1,
-    cacheExpireTime: getenv("CAF_BATCH_WORKER_CACHE_EXPIRE_TIME") || 120,
+    cacheExpireTime: 1,
     returnValueBehaviour: getenv("CAF_BATCH_WORKER_ERROR_QUEUE") ? "RETURN_NONE" : undefined
 });
