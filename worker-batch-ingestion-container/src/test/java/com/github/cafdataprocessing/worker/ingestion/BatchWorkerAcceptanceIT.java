6a31116a4456121a8a3de40d3406d213b32ad5aa
michael-bryson <michael.bryson@microfocus.com>
2019-11-29 16:55:24 +0000

    Remove rogue copyright statement

--- worker-batch-ingestion-container/src/test/java/com/github/cafdataprocessing/worker/ingestion/BatchWorkerAcceptanceIT.java
+++ worker-batch-ingestion-container/src/test/java/com/github/cafdataprocessing/worker/ingestion/BatchWorkerAcceptanceIT.java
@@ -48,25 +48,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-
-/*
- * Copyright 2019-2018 Micro Focus or one of its affiliates.
- *
- * The only warranties for products and services of Micro Focus and its
- * affiliates and licensors ("Micro Focus") are set forth in the express
- * warranty statements accompanying such products and services. Nothing
- * herein should be construed as constituting an additional warranty.
- * Micro Focus shall not be liable for technical or editorial errors or
- * omissions contained herein. The information contained herein is subject
- * to change without notice.
- *
- * Contains Confidential Information. Except as specifically indicated
- * otherwise, a valid license is required for possession, use or copying.
- * Consistent with FAR 12.211 and 12.212, Commercial Computer Software,
- * Computer Software Documentation, and Technical Data for Commercial
- * Items are licensed to the U.S. Government under vendor's standard
- * commercial license.
- */
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @DisplayName("IT for IngestionBatchWorkerPlugin")
 @Slf4j
